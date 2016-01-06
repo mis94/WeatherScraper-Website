e4a3462f4bd50c3cfa145b5cdfe9582c52ba91dd
mis94 <mis1494@gmail.com>
2016-01-06 21:52:53 +0200

    Separate contents to different files

---
+++ Script.js
@@ -0,0 +1,18 @@
+﻿$("#findMyWeather").click(function (event) {
+    event.preventDefault();
+    $(".alert").hide();
+    if ($("#city").val() != "") {
+        $.get("scraper.php?city=" + $("#city").val(), function (data) {
+            // data is what the $.get has obtained for us from the specified parameter
+            if (data == "") {
+                $("#fail").fadeIn();
+            } else {
+                $(".alert-success").html(data).fadeIn();
+            }
+
+        });
+    } else {
+        $("#noCity").fadeIn();
+    }
+
+});
